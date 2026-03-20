8e3d0b41f0daa62905d9769ce492d338c47ceb71
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-03-20 10:20:52 +0530

    create the rest in the plugins by ganesh

---
+++ src/main/java/com/vastpro/sphinx/rest/SphinxRest.java
@@ -0,0 +1,9 @@
+package com.vastpro.sphinx.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/sphinx")
+public class SphinxRest extends Application{
+
+}
