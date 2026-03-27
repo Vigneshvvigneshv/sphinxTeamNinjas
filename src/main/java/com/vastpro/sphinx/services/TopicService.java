62265eac8e7f1e9247c07f05b9315510fe196db8
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-03-27 13:16:22 +0530

    update message in topic service

--- src/main/java/com/vastpro/sphinx/services/TopicService.java
+++ src/main/java/com/vastpro/sphinx/services/TopicService.java
@@ -111,7 +111,7 @@ public class TopicService {
 	                                                  .queryList();
 	        
 	        if(topicList.size()==0) {
-	        	return ServiceUtil.returnSuccess("No topic available");
+	        	return ServiceUtil.returnError("No topic available");
 	        }
 	        
 	        result.put("topicList", topicList);
