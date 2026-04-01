9ca7993e7c43d66c8134c6f1ba7334ed8e45bdae
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-04-01 13:25:01 +0530

    exam assigned to the user completed by ganesh

--- src/main/java/com/vastpro/sphinx/services/ExamAssignToUserService.java
+++ src/main/java/com/vastpro/sphinx/services/ExamAssignToUserService.java
@@ -12,69 +12,76 @@ import org.apache.ofbiz.service.LocalDispatcher;
 import org.apache.ofbiz.service.ServiceUtil;
 
 public class ExamAssignToUserService {
-	
-	public static Map<String,Object> assignExam(DispatchContext context, Map<String,Object> input){
-		LocalDispatcher dispatcher=context.getDispatcher();
-		Delegator delegator=context.getDelegator();
-		
+
+	public static Map<String, Object> assignExam(DispatchContext context, Map<String, Object> input) {
+		LocalDispatcher dispatcher = context.getDispatcher();
+		Delegator delegator = context.getDelegator();
+
 		try {
-			GenericValue partyIdAlreadyExits = EntityQuery.use(delegator).from("UserLogin").where("partyId",input.get("partyId")).queryFirst();
-			if(partyIdAlreadyExits==null) {
+			GenericValue partyIdAlreadyExits = EntityQuery.use(delegator).from("UserLogin")
+					.where("partyId", input.get("partyId")).queryFirst();
+			if (partyIdAlreadyExits == null) {
 				return ServiceUtil.returnError("User not found");
 			}
-			GenericValue examIdAlreadyExits=EntityQuery.use(delegator).from("ExamMaster").where("examId",input.get("examId")).queryFirst();
-			if(examIdAlreadyExits==null) {
+			GenericValue examIdAlreadyExits = EntityQuery.use(delegator).from("ExamMaster")
+					.where("examId", input.get("examId")).queryFirst();
+			if (examIdAlreadyExits == null) {
 				return ServiceUtil.returnError("Exam not found");
 			}
-			
-			String examAssignId=delegator.getNextSeqId("ExamAssignMaster");
-			input.put("examAssignId",examAssignId);
-			Map<String, Object> result = dispatcher.runSync("assignExam",input );
-			
-			if(ServiceUtil.isError(result)) {
+
+			String examAssignId = delegator.getNextSeqId("ExamAssignMaster");
+			input.put("examAssignId", examAssignId);
+			Map<String, Object> result = dispatcher.runSync("assignExam", input);
+
+			if (ServiceUtil.isError(result)) {
 				return ServiceUtil.returnError("Error, occur during assing the Exam");
 			}
 			return result;
-			 
-		} catch (GenericEntityException |GenericServiceException  e) {
+
+		} catch (GenericEntityException | GenericServiceException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return ServiceUtil.returnError("Error, occur during assing the Exam"+e.getMessage());
-		}
-	}
-	
-	public static Map<String,Object> removeAssignedExam(DispatchContext context, Map<String,Object> input){
-		LocalDispatcher dispatcher=context.getDispatcher();
-		Delegator delegator=context.getDelegator();
-		
-		try {
-			
-			String partyId=(String)input.get("partyId");
-			String examId=(String)input.get("examId");
-			GenericValue partyIdAlreadyExits = EntityQuery.use(delegator).from("UserLogin").where("partyId",partyId).queryFirst();
-			if(partyIdAlreadyExits==null) {
-				return ServiceUtil.returnError("User not found");
-			}
-			GenericValue examIdAlreadyExits=EntityQuery.use(delegator).from("ExamMaster").where("examId",examId).queryFirst();
-			if(examIdAlreadyExits==null) {
-				return ServiceUtil.returnError("Exam not found");
-			}
-			
-			GenericValue examAssignId=EntityQuery.use(delegator).from("ExamAssignMaster").where("partyId",partyId,"examId",examId).queryFirst();
-			String assignId=examAssignId.getString("userLoginId");
-			Map<String, Object> result = dispatcher.runSync("removeAssignedValue",Map.of("examAssignId", assignId) );
-			
-			if(ServiceUtil.isError(result)) {
-//				return ServiceUtil.returnError("Error, occur during remove the assigned Exam");
-				return ServiceUtil.returnError((String)result.get("errorMessage"));
-			}
-			return result;
-			 
-		} catch (GenericEntityException |GenericServiceException  e) {
-			e.printStackTrace();
-			return ServiceUtil.returnError("Error, occur during remove the assigned Exam"+e.getMessage());
+			return ServiceUtil.returnError("Error, occur during assing the Exam" + e.getMessage());
 		}
 	}
 
+	public static Map<String, Object> removeAssignedExam(DispatchContext context, Map<String, Object> input) {
+		LocalDispatcher dispatcher = context.getDispatcher();
+		Delegator delegator = context.getDelegator();
+
+		try {
+
+			String partyId = (String) input.get("partyId");
+			String examId = (String) input.get("examId");
+			GenericValue partyIdAlreadyExits = EntityQuery.use(delegator).from("UserLogin").where("partyId", partyId)
+					.queryFirst();
+			if (partyIdAlreadyExits == null) {
+				return ServiceUtil.returnError("User not found");
+			}
+			GenericValue examIdAlreadyExits = EntityQuery.use(delegator).from("ExamMaster").where("examId", examId)
+					.queryFirst();
+			if (examIdAlreadyExits == null) {
+				return ServiceUtil.returnError("Exam not found");
+			}
+
+			GenericValue examAssignId = EntityQuery.use(delegator).from("ExamAssignMaster")
+					.where("partyId", partyId, "examId", examId).queryFirst();
+			if (examAssignId == null) {
+				return ServiceUtil.returnError("Exam is already not Assigned to the user");
+			}
+			String assignId = examAssignId.getString("examAssignId");
+			Map<String, Object> result = dispatcher.runSync("removeAssignedValue", Map.of("examAssignId", assignId));
+
+			if (ServiceUtil.isError(result)) {
+				return ServiceUtil.returnError("Error, occur during remove the assigned Exam");
+//				return ServiceUtil.returnError((String)result.get("errorMessage"));
+			}
+			return ServiceUtil.returnSuccess("Exam removed successfully");
+
+		} catch (GenericEntityException | GenericServiceException e) {
+			e.printStackTrace();
+			return ServiceUtil.returnError("Error, occur during remove the assigned Exam" + e.getMessage());
+		}
+	}
 
 }
