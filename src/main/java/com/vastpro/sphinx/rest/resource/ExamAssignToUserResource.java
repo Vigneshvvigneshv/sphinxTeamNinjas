264ee50841dc2611ce89b05aed5694c74945f75b
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-04-02 12:55:34 +0530

    exam assign to user(assign,remove,increaseAttempts) by ganesh

--- src/main/java/com/vastpro/sphinx/rest/resource/ExamAssignToUserResource.java
+++ src/main/java/com/vastpro/sphinx/rest/resource/ExamAssignToUserResource.java
@@ -5,14 +5,17 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.ofbiz.service.LocalDispatcher;
+import org.apache.ofbiz.service.ServiceUtil;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -30,6 +33,8 @@ public class ExamAssignToUserResource {
 			Map<String,Object> input=new HashMap<String, Object>();
 			input.put("partyId", request.getAttribute("partyId"));
 			input.put("examId", request.getAttribute("examId"));
+			input.put("allowedAttempts", request.getAttribute("allowedAttempts"));
+			input.put("timeoutDays", request.getAttribute("timeoutDays"));
 			Map<String,Object> result=dispatcher.runSync("assignExamOwn",input);
 			return Response.ok(result).build();
 		}catch(Exception e) {
@@ -53,4 +58,23 @@ public class ExamAssignToUserResource {
 			return Response.status(500).entity(Map.of("error", e.getMessage())).build();
 		}
 	}
+	
+	@PUT
+	@Path("/increaseattempts")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response increaseAttempts(@Context HttpServletRequest request,@Context HttpServletResponse response){
+		LocalDispatcher dispatcher=(LocalDispatcher)request.getAttribute("dispatcher");
+		try {
+			
+			Map<String,Object> input=new HashMap<String, Object>();
+			input.put("partyId", request.getAttribute("partyId"));
+			input.put("examId", request.getAttribute("examId"));
+			Map<String,Object> result=dispatcher.runSync("increaseAttemptsOwn", input);
+			return Response.ok(result).build();
+			
+		}catch(Exception e) {
+			return Response.status(500).entity(Map.of("error",e.getMessage())).build();
+		}
+	} 
 }
--- src/main/java/com/vastpro/sphinx/services/ExamAssignToUserService.java
+++ src/main/java/com/vastpro/sphinx/services/ExamAssignToUserService.java
@@ -2,6 +2,7 @@ package com.vastpro.sphinx.services;
 
 import java.util.Map;
 
+import org.apache.ofbiz.base.util.Debug;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
@@ -28,9 +29,18 @@ public class ExamAssignToUserService {
 			if (examIdAlreadyExits == null) {
 				return ServiceUtil.returnError("Exam not found");
 			}
-
-			String examAssignId = delegator.getNextSeqId("ExamAssignMaster");
-			input.put("examAssignId", examAssignId);
+			try {
+			input.put("allowedAttempts",Long.valueOf((String) input.get("allowedAttempts")));
+			}catch(NumberFormatException e) {
+				return ServiceUtil.returnError("Attempts should be number");
+			}
+			
+			try {
+				input.put("timeoutDays",Long.valueOf((String) input.get("timeoutDays")));
+				}catch(NumberFormatException e) {
+					return ServiceUtil.returnError("Days should be number");
+				}
+			input.put("noOfAttempts", 0);
 			Map<String, Object> result = dispatcher.runSync("assignExam", input);
 
 			if (ServiceUtil.isError(result)) {
@@ -41,10 +51,12 @@ public class ExamAssignToUserService {
 		} catch (GenericEntityException | GenericServiceException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			Debug.logError(e.getMessage(), ExamAssignToUserService.class.getName());
 			return ServiceUtil.returnError("Error, occur during assing the Exam" + e.getMessage());
 		}
 	}
 
+	
 	public static Map<String, Object> removeAssignedExam(DispatchContext context, Map<String, Object> input) {
 		LocalDispatcher dispatcher = context.getDispatcher();
 		Delegator delegator = context.getDelegator();
@@ -53,24 +65,23 @@ public class ExamAssignToUserService {
 
 			String partyId = (String) input.get("partyId");
 			String examId = (String) input.get("examId");
-			GenericValue partyIdAlreadyExits = EntityQuery.use(delegator).from("UserLogin").where("partyId", partyId)
+			GenericValue partyIdAlreadyExits = EntityQuery.use(delegator).from("PartyExamRelationship").where("partyId", partyId)
 					.queryFirst();
 			if (partyIdAlreadyExits == null) {
 				return ServiceUtil.returnError("User not found");
 			}
-			GenericValue examIdAlreadyExits = EntityQuery.use(delegator).from("ExamMaster").where("examId", examId)
+			GenericValue examIdAlreadyExits = EntityQuery.use(delegator).from("PartyExamRelationship").where("examId", examId)
 					.queryFirst();
 			if (examIdAlreadyExits == null) {
 				return ServiceUtil.returnError("Exam not found");
 			}
 
-			GenericValue examAssignId = EntityQuery.use(delegator).from("ExamAssignMaster")
+			GenericValue examAssignId = EntityQuery.use(delegator).from("PartyExamRelationship")
 					.where("partyId", partyId, "examId", examId).queryFirst();
 			if (examAssignId == null) {
 				return ServiceUtil.returnError("Exam is already not assigned to the user");
 			}
-			String assignId = examAssignId.getString("examAssignId");
-			Map<String, Object> result = dispatcher.runSync("removeAssignedValue", Map.of("examAssignId", assignId));
+			Map<String, Object> result = dispatcher.runSync("removeAssignedValue",input );
 
 			if (ServiceUtil.isError(result)) {
 				return ServiceUtil.returnError("Error, occur during remove the assigned Exam");
@@ -83,5 +94,48 @@ public class ExamAssignToUserService {
 			return ServiceUtil.returnError("Error, occur during remove the assigned Exam" + e.getMessage());
 		}
 	}
+	
+	public static Map<String,Object> increaseAttempts(DispatchContext context,Map<String,Object> input){
+		LocalDispatcher dispatcher = context.getDispatcher();
+		Delegator delegator = context.getDelegator();
 
+		try {
+			GenericValue partyIdAlreadyExits = EntityQuery.use(delegator).from("PartyExamRelationship")
+					.where("partyId", input.get("partyId")).queryFirst();
+			if (partyIdAlreadyExits == null) {
+				return ServiceUtil.returnError("User not found");
+			}
+			GenericValue examIdAlreadyExits = EntityQuery.use(delegator).from("PartyExamRelationship")
+					.where("examId", input.get("examId")).queryFirst();
+			if (examIdAlreadyExits == null) {
+				return ServiceUtil.returnError("Exam not found");
+			}
+			
+			GenericValue getRecords=EntityQuery.use(delegator).from("PartyExamRelationship")
+					.where("partyId",input.get("partyId"),"examId", input.get("examId")).queryFirst();
+			if(getRecords==null) {
+				return ServiceUtil.returnError("Exam is already not assigned to the user");
+			}
+			
+			Long numberOfAttempts = getRecords.getLong("noOfAttempts");
+			Long allowAttempts =getRecords.getLong("allowedAttempts");
+		
+			if(numberOfAttempts<=allowAttempts) {
+				input.put("noOfAttempts", (numberOfAttempts+1));
+			}else {
+				return ServiceUtil.returnError("User reached a maximum attempts");
+			}
+			
+			Map<String,Object> result=dispatcher.runSync("increaseAttempts", input);
+			
+			if(ServiceUtil.isError(result)) {
+				Debug.logError((String)result.get("errorMessage"), ExamAssignToUserService.class.getName());
+				return ServiceUtil.returnError("Error, occur launch the exam"); 
+			}
+			return result;
+		}catch (GenericEntityException | GenericServiceException e) {
+			Debug.logError(e.getMessage(), ExamAssignToUserService.class.getName());
+			return ServiceUtil.returnError("Error, occur launch the exam");
+		}
+	}
 }
