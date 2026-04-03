1ac6d19662f4a36634ca4f22c8de01e62b65a970
Ussainfaizul18 <ussainfaizul1803@gmail.com>
2026-04-03 15:55:14 +0530

    vicky commited

--- src/main/java/com/vastpro/sphinx/rest/resource/QuestionResource.java
+++ src/main/java/com/vastpro/sphinx/rest/resource/QuestionResource.java
@@ -25,43 +25,37 @@ import javax.ws.rs.core.Response;
 
 import org.apache.ofbiz.base.util.UtilMisc;
 import org.apache.ofbiz.entity.Delegator;
-
 import org.apache.ofbiz.service.GenericServiceException;
 import org.apache.ofbiz.service.LocalDispatcher;
 import org.apache.ofbiz.service.ServiceContainer;
 import org.apache.ofbiz.service.ServiceUtil;
 
-
 import com.vastpro.sphinx.util.ConvertValue;
-
-
 
 @Path("/question")
 public class QuestionResource {
-	
-	
+
 	@Context
 	private HttpServletRequest request;
 
 	@Context
-	private ServletContext servletContext; 
+	private ServletContext servletContext;
 
-	
 	@POST
 	@Path("/createquestion")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response createQuestion(@Context HttpServletRequest request, @Context HttpServletResponse response) {
-		
-		LocalDispatcher dispatcher=(LocalDispatcher)request.getAttribute("dispatcher");
-		if(dispatcher==null) {
-			dispatcher=ServiceContainer.getLocalDispatcher("sphinx", (Delegator)request.getAttribute("delegator"));
+
+		LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+		if (dispatcher == null) {
+			dispatcher = ServiceContainer.getLocalDispatcher("sphinx", (Delegator) request.getAttribute("delegator"));
 		}
-		
-		try {	
-			
-			Map<String,Object> input= new HashMap<String, Object>();
-			
+
+		try {
+
+			Map<String, Object> input = new HashMap<String, Object>();
+
 			input.put("questionDetail", request.getAttribute("questionDetail"));
 			input.put("optionA", request.getAttribute("optionA"));
 			input.put("optionB", request.getAttribute("optionB"));
@@ -74,49 +68,47 @@ public class QuestionResource {
 			input.put("answerValue", request.getAttribute("answerValue"));
 			input.put("topicId", request.getAttribute("topicId"));
 			input.put("negativeMarkValue", request.getAttribute("negativeMarkValue"));
-		
-			
+
 			Map<String, Object> result = dispatcher.runSync("createQuestionService", input);
 			return Response.ok(result).build();
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(500).entity(Map.of("error", e.getMessage())).build();
 		}
-		
+
 	}
-	
-	
+
 	@PUT
 	@Path("/updatequestion")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updateQuestion(@Context HttpServletRequest request,@Context HttpServletResponse response) {
-	    
-		//getting dispatcher from request
-		LocalDispatcher dispatcher=(LocalDispatcher)request.getAttribute("dispatcher");
-		if(dispatcher==null) {
-			dispatcher=ServiceContainer.getLocalDispatcher("sphinx", (Delegator)request.getAttribute("delegator"));
+	public Response updateQuestion(@QueryParam("questionId") String questionIdStr, @Context HttpServletRequest request,
+					@Context HttpServletResponse response) {
+
+		// getting dispatcher from request
+		LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+		if (dispatcher == null) {
+			dispatcher = ServiceContainer.getLocalDispatcher("sphinx", (Delegator) request.getAttribute("delegator"));
 		}
-		
+
 		Map<String, Object> result = new HashMap<>();
-	    try {
-	    	
-	        // questionId must be sent by frontend  
-	        String questionIdStr = (String) request.getAttribute("questionId");
-	        if (questionIdStr == null) {
-	        	result.put("status",  "ERROR");
-	        	result.put("message", "questionId is required");
-	        	return Response.status(400).entity(result).build();
-	        }
-	        
-	        
-	        Long questionId = Long.valueOf(questionIdStr);
-	        
-	        Map<String,Object>input=new HashMap<String, Object>();
-	        
-	        input.put("questionId", questionId);
-	        input.put("questionDetail", request.getAttribute("questionDetail"));
+		try {
+
+			// questionId must be sent by frontend
+			// String questionIdStr = (String) request.getAttribute("questionId");
+			if (questionIdStr == null) {
+				result.put("status", "ERROR");
+				result.put("message", "questionId is required");
+				return Response.status(400).entity(result).build();
+			}
+
+			Long questionId = Long.valueOf(questionIdStr);
+
+			Map<String, Object> input = new HashMap<String, Object>();
+
+			input.put("questionId", questionId);
+			input.put("questionDetail", request.getAttribute("questionDetail"));
 			input.put("optionA", request.getAttribute("optionA"));
 			input.put("optionB", request.getAttribute("optionB"));
 			input.put("optionC", request.getAttribute("optionC"));
@@ -129,146 +121,137 @@ public class QuestionResource {
 			input.put("topicId", request.getAttribute("topicId"));
 			input.put("negativeMarkValue", request.getAttribute("negativeMarkValue"));
 
-			
-			System.out.println("negativeMarkValue"+request.getAttribute("negativeMarkValue"));
-			System.out.println("questionTypeId"+request.getAttribute("questionTypeId"));
-	       
-	        // Call service
-	        Map<String, Object> serviceResult  = dispatcher.runSync("updateQuestionMaster", input);
+			System.out.println("negativeMarkValue" + request.getAttribute("negativeMarkValue"));
+			System.out.println("questionTypeId" + request.getAttribute("questionTypeId"));
 
-	        if (ServiceUtil.isError(serviceResult)) {
-	        	result.put("responseMessage",  "ERROR");
-	        	result.put("message", ServiceUtil.getErrorMessage(serviceResult));
-	            return Response.status(500).entity(result ).build();
-	        }
+			// Call service
+			Map<String, Object> serviceResult = dispatcher.runSync("updateQuestionMaster", input);
 
-	        result.put("responseMessage","SUCCESS");
-	        result.put("message","Question updated successfully");
-	        return Response.ok(result).build();
-
-	    } catch (Exception e) {
-	    	result.put("responseMessage",  "ERROR");
-	    	result.put("message", e.getMessage());
-	        return Response.status(500).entity(result).build();
-	    }
-	}
-	
-	@DELETE
-	@Path("/deletequestion")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response deleteQuestion(@Context HttpServletRequest request,@Context HttpServletResponse response) {
-	    Map<String, Object> result = ServiceUtil.returnSuccess();
-	    
-	  //getting dispatcher from request
-	  		LocalDispatcher dispatcher=(LocalDispatcher)request.getAttribute("dispatcher");
-	  		if(dispatcher==null) {
-	  			dispatcher=ServiceContainer.getLocalDispatcher("sphinx", (Delegator)request.getAttribute("delegator"));
-	  		}
-	    
-	    try {
-	        String questionIdStr=String.valueOf(request.getAttribute("questionId"));
-	        
-	        if (questionIdStr == null || questionIdStr.trim().isEmpty()) {
-	        	result.put("responseMessage",  "ERROR");
-	        	result.put("message", "questionId is required");
-	        	return Response.status(400).entity(result).build();
-	        }
-	        
-	        Long questionId=Long.valueOf(questionIdStr);   
-	        
-	        Map<String,Object>input=new HashMap<String, Object>();
-	        input.put("questionId", questionId);
-
-	        // Call service
-	        Map<String, Object> serviceResult = dispatcher.runSync("deleteQuestionMaster",input);
-
-	        if (ServiceUtil.isError(serviceResult)) {
-	            result.put("responseMessage",  "ERROR");
-	            
-	            return Response.status(500).entity(result).build();
-	        }
-
-	        result.put("responseMessage","SUCCESS");
-	        result.put("message", "question deleted Successfully");
-	        return Response.ok(result).build();
-
-	    } catch (Exception e) {
-	        result.put("responseMessage",  "ERROR");
-	        result.put("message", e.getMessage());
-	        return Response.status(500).entity(result).build();
-	    }
-	}
-	
-	
-	
-	@GET
-	@Path("/getquestionsbytopic")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response getQuestionsByTopic(@Context HttpServletRequest request,@QueryParam("topicId") String topicId, @QueryParam("pageNo") String pageNoStr,@QueryParam("pageSize") String pageSizeStr ){
-		
-		 //getting dispatcher from request
-  		LocalDispatcher dispatcher=(LocalDispatcher)request.getAttribute("dispatcher");
-  		if(dispatcher==null) {
-  			dispatcher=ServiceContainer.getLocalDispatcher("sphinx", (Delegator)request.getAttribute("delegator"));
-  		}
-		
-		Map<String,Object>result=new HashMap<>();
-		try {
-			
-			if(topicId==null || topicId.trim().isEmpty()) {
-				result.put("responseMessage","ERROR");
-				result.put("message","TopicId is Required");
-				return Response.status(400).entity(result).build();
-			}
-			
-			Integer pageNo=ConvertValue.toInteger(pageNoStr) ;
-			Integer pageSize=ConvertValue.toInteger(pageSizeStr);
-			
-			
-			Map<String,Object>serviceCtx=new HashMap<>();
-			
-			serviceCtx.put("topicId", topicId);
-			serviceCtx.put("pageNo", pageNo);
-			serviceCtx.put("pageSize",pageSize);	
-			
-			
-			Map<String,Object>serviceResult=dispatcher.runSync("getQuestionsByTopic",serviceCtx);
-			
-		
-			if(ServiceUtil.isError(serviceResult)) {
+			if (ServiceUtil.isError(serviceResult)) {
 				result.put("responseMessage", "ERROR");
 				result.put("message", ServiceUtil.getErrorMessage(serviceResult));
 				return Response.status(500).entity(result).build();
 			}
-			
+
+			result.put("responseMessage", "SUCCESS");
+			result.put("message", "Question updated successfully");
+			return Response.ok(result).build();
+
+		} catch (Exception e) {
+			result.put("responseMessage", "ERROR");
+			result.put("message", e.getMessage());
+			return Response.status(500).entity(result).build();
+		}
+	}
+
+	@DELETE
+	@Path("/deletequestion")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response deleteQuestion(@Context HttpServletRequest request, @Context HttpServletResponse response) {
+		Map<String, Object> result = ServiceUtil.returnSuccess();
+
+		// getting dispatcher from request
+		LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+		if (dispatcher == null) {
+			dispatcher = ServiceContainer.getLocalDispatcher("sphinx", (Delegator) request.getAttribute("delegator"));
+		}
+
+		try {
+			String questionIdStr = String.valueOf(request.getAttribute("questionId"));
+
+			if (questionIdStr == null || questionIdStr.trim().isEmpty()) {
+				result.put("responseMessage", "ERROR");
+				result.put("message", "questionId is required");
+				return Response.status(400).entity(result).build();
+			}
+
+			Long questionId = Long.valueOf(questionIdStr);
+
+			Map<String, Object> input = new HashMap<String, Object>();
+			input.put("questionId", questionId);
+
+			// Call service
+			Map<String, Object> serviceResult = dispatcher.runSync("deleteQuestionMaster", input);
+
+			if (ServiceUtil.isError(serviceResult)) {
+				result.put("responseMessage", "ERROR");
+
+				return Response.status(500).entity(result).build();
+			}
+
+			result.put("responseMessage", "SUCCESS");
+			result.put("message", "question deleted Successfully");
+			return Response.ok(result).build();
+
+		} catch (Exception e) {
+			result.put("responseMessage", "ERROR");
+			result.put("message", e.getMessage());
+			return Response.status(500).entity(result).build();
+		}
+	}
+
+	@GET
+	@Path("/getquestionsbytopic")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getQuestionsByTopic(@Context HttpServletRequest request, @QueryParam("topicId") String topicId,
+					@QueryParam("pageNo") String pageNoStr, @QueryParam("pageSize") String pageSizeStr) {
+
+		// getting dispatcher from request
+		LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+		if (dispatcher == null) {
+			dispatcher = ServiceContainer.getLocalDispatcher("sphinx", (Delegator) request.getAttribute("delegator"));
+		}
+
+		Map<String, Object> result = new HashMap<>();
+		try {
+
+			if (topicId == null || topicId.trim().isEmpty()) {
+				result.put("responseMessage", "ERROR");
+				result.put("message", "TopicId is Required");
+				return Response.status(400).entity(result).build();
+			}
+
+			Integer pageNo = ConvertValue.toInteger(pageNoStr);
+			Integer pageSize = ConvertValue.toInteger(pageSizeStr);
+
+			Map<String, Object> serviceCtx = new HashMap<>();
+
+			serviceCtx.put("topicId", topicId);
+			serviceCtx.put("pageNo", pageNo);
+			serviceCtx.put("pageSize", pageSize);
+
+			Map<String, Object> serviceResult = dispatcher.runSync("getQuestionsByTopic", serviceCtx);
+
+			if (ServiceUtil.isError(serviceResult)) {
+				result.put("responseMessage", "ERROR");
+				result.put("message", ServiceUtil.getErrorMessage(serviceResult));
+				return Response.status(500).entity(result).build();
+			}
+
 			result.put("responseMessage", "SUCCESS");
 			result.put("topicId", serviceResult.get("topicId"));
 			result.put("topicName", serviceResult.get("topicName"));
 			result.put("totalCount", serviceResult.get("totalCount"));
 			result.put("questionList", serviceResult.get("questionList"));
-			
 
-			result.put("pageNo",serviceResult.get("pageNo"));
-			result.put("pageSize",serviceResult.get("pageSize"));
+			result.put("pageNo", serviceResult.get("pageNo"));
+			result.put("pageSize", serviceResult.get("pageSize"));
 			result.put("totalPages", serviceResult.get("totalPages"));
 			result.put("hasNext", serviceResult.get("hasNext"));
-			result.put("hasPrevious",serviceResult.get("hasPrevious"));
-			
-			
+			result.put("hasPrevious", serviceResult.get("hasPrevious"));
+
 			return Response.ok(result).build();
-			
-			
-		}catch(GenericServiceException e) {
+
+		} catch (GenericServiceException e) {
 			result.put("responseMessage", "ERROR");
 			result.put("message", e.getMessage());
 			e.printStackTrace();
 			return Response.status(500).entity(result).build();
 		}
 	}
-	
-	
+
 	@GET
 	@Path("/downloadTemplate")
 	@Produces("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
@@ -291,7 +274,7 @@ public class QuestionResource {
 		}
 
 	}
-	
+
 	@POST
 	@Path("/upload")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -323,79 +306,74 @@ public class QuestionResource {
 
 		} catch (IOException | ServletException e) {
 			e.printStackTrace();
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-							.entity(ServiceUtil.returnError("Unexpected error occured!")).build();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ServiceUtil.returnError("Unexpected error occured!"))
+							.build();
 		}
 
 		LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
-		
+
 		try {
 			Map<String, Object> result = dispatcher.runSync("uploadBulkQuestion", UtilMisc.toMap("file", buffer));
 			if (result.get("responseMessage") != null && result.get("responseMessage").equals("error")) {
 				return Response.status(Response.Status.BAD_REQUEST).entity(result).build();
-			}
-			else {
+			} else {
 				result.put("successMessage", "Questions uploaded successfully!");
 			}
 
 			return Response.ok().entity(result).build();
 
 		} catch (GenericServiceException e) {
-			
+
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
 							.entity(ServiceUtil.returnError("Unexpected error occured, try again after sometime!")).build();
 		}
 
 	}
-	
-	
+
 	@GET
 	@Path("/getquestionbyid")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getQuetionsById(@QueryParam("questionId") String questionIdStr, @Context HttpServletRequest request,@Context HttpServletResponse response) {
-		
-		 //getting dispatcher from request
-  		LocalDispatcher dispatcher=(LocalDispatcher)request.getAttribute("dispatcher");
-  		if(dispatcher==null) {
-  			dispatcher=ServiceContainer.getLocalDispatcher("sphinx", (Delegator)request.getAttribute("delegator"));
-  		}
-		
-  		
-  		Map<String,Object>result=new HashMap<>();
-  		
-  		
-  		try {
-//  			String questionIdStr=(String)request.getAttribute("questionId");
-  			
-  			Long questionId=Long.valueOf(questionIdStr);
-  			
-  			Map<String,Object>input=new HashMap<>();
-  			
-  			input.put("questionId", questionId);
-  			
-  			Map<String,Object>ServiceResult=dispatcher.runSync("getQuestionById", input);
-  			
-  			
-  			
-  			if(ServiceUtil.isError(ServiceResult)) {
-  				result.put("responseMessage","ERROR");
-  				result.put("message", ServiceUtil.getErrorMessage(ServiceResult));
-  				result.put("question",null);
-  				
-  				return Response.status(500).entity(result).build();
-  			}
-  			
-  			result.put("responseMessage", "Success");
-  			result.put("message","Question Fetched SuccessFully");
-  			result.put("question", ServiceResult.get("question"));
-  			return Response.ok().entity(result).build();
-  		}catch(GenericServiceException e) {
-  			result.put("responseMesage", "ERROR");
-  			result.put("message", "Failed to Fetch the question");
-  			
-  			return Response.ok().entity(result).build();
-  							
-  		}
+	public Response getQuetionsById(@QueryParam("questionId") String questionIdStr, @Context HttpServletRequest request,
+					@Context HttpServletResponse response) {
+
+		// getting dispatcher from request
+		LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+		if (dispatcher == null) {
+			dispatcher = ServiceContainer.getLocalDispatcher("sphinx", (Delegator) request.getAttribute("delegator"));
+		}
+
+		Map<String, Object> result = new HashMap<>();
+
+		try {
+			// String questionIdStr=(String)request.getAttribute("questionId");
+
+			Long questionId = Long.valueOf(questionIdStr);
+
+			Map<String, Object> input = new HashMap<>();
+
+			input.put("questionId", questionId);
+
+			Map<String, Object> ServiceResult = dispatcher.runSync("getQuestionById", input);
+
+			if (ServiceUtil.isError(ServiceResult)) {
+				result.put("responseMessage", "ERROR");
+				result.put("message", ServiceUtil.getErrorMessage(ServiceResult));
+				result.put("question", null);
+
+				return Response.status(500).entity(result).build();
+			}
+
+			result.put("responseMessage", "Success");
+			result.put("message", "Question Fetched SuccessFully");
+			result.put("question", ServiceResult.get("question"));
+			return Response.ok().entity(result).build();
+		} catch (GenericServiceException e) {
+			result.put("responseMesage", "ERROR");
+			result.put("message", "Failed to Fetch the question");
+
+			return Response.ok().entity(result).build();
+
+		}
 	}
 }
