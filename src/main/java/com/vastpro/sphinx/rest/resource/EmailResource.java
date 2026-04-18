a45ebb77a0d32796c62ef248f1cceb7733e336a7
Ussainfaizul18 <ussainfaizul1803@gmail.com>
2026-04-18 09:09:38 +0530

    vicky latest commit

--- src/main/java/com/vastpro/sphinx/rest/resource/EmailResource.java
+++ src/main/java/com/vastpro/sphinx/rest/resource/EmailResource.java
@@ -5,15 +5,30 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.service.GenericServiceException;
 import org.apache.ofbiz.service.LocalDispatcher;
 import org.apache.ofbiz.service.ServiceContainer;
+import org.apache.ofbiz.service.ServiceUtil;
 
+
+@Path("/email")
 public class EmailResource {
 
+	
+	@POST
+	@Path("/send-email")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response emailResource(@Context HttpServletRequest request, @Context HttpServletResponse response) {
 		
 		LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
@@ -33,13 +48,20 @@ public class EmailResource {
 			input.put("examId",examId);
 			
 			Map<String,Object>serviceResult=dispatcher.runSync("EmailService",input );
-			return null;
-		}catch(Exception e) {
-			return null;
-		}
+			
+			if(ServiceUtil.isError(serviceResult)) {
+				result.put("status", "Error");
+				result.put("errorMessage", ServiceUtil.getErrorMessage(serviceResult));
 
-		
-		
-		
+				return Response.status(200).entity(result).build();
+				}
+				return Response.ok().entity(serviceResult).build();
+		}catch(GenericServiceException e) {
+			e.printStackTrace();
+			result.put("status", "Error");
+			result.put("errorMessage", e.getMessage());
+
+			return Response.status(200).entity(result).build();
+		}	
 	}
 }
---
+++ src/main/java/com/vastpro/sphinx/services/EmailService.java
@@ -0,0 +1,93 @@
+package com.vastpro.sphinx.services;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ofbiz.base.util.UtilMisc;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.util.EntityQuery;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.GenericServiceException;
+import org.apache.ofbiz.service.LocalDispatcher;
+import org.apache.ofbiz.service.ServiceUtil;
+
+import com.vastpro.sphinx.util.PasswordUtil;
+
+public class EmailService {
+
+public static Map<String,Object>sendExamNotification(DispatchContext dctx,Map<String,Object>context){
+		
+		Delegator delegator=dctx.getDelegator();
+		
+		LocalDispatcher dispatcher = dctx.getDispatcher();
+		
+		try {
+			
+			String examId=(String) context.get("examId");
+			
+			if(examId==null) {
+				return ServiceUtil.returnError("examId is required");
+			}	
+			
+			GenericValue examRecord=EntityQuery.use(delegator).from("ExamMaster").where("examId",examId).queryOne();
+			
+			if(examRecord==null) {
+				return ServiceUtil.returnError("Invalid Exam Details! No Records found!");
+			}
+			
+			
+			String examName=examRecord.getString("examName");
+			
+			String password=PasswordUtil.generatePassword();
+			
+			try {
+				dispatcher.runAsync("PasswordChangesAuto", UtilMisc.toMap("passwordChangesAuto",password));
+			}catch(GenericServiceException e) {
+				e.printStackTrace();
+				return ServiceUtil.returnError("Failed To Send an Exam Notification "+e.getMessage());
+			}
+			
+			List<GenericValue> assignedUsersListsWithEmails = EntityQuery.use(delegator).from("UserDetailsForEmail")
+							.where("contactMechTypeId", "EMAIL_ADDRESS", "examId", examRecord.get("examId"))
+							.queryList();
+			
+			
+			if (assignedUsersListsWithEmails.isEmpty()) {
+				return ServiceUtil.returnError("No assigned users found for the exam.");
+			}
+			
+			
+			Map<String, Object> emailContext = new HashMap<>();
+			emailContext.put("subject", "Exam Assignment and Access Details");
+			
+			String emailBody = "Dear Candidate,\n\n" + "You have been successfully assigned to the following examination: " + examName
+							+ "\n\nPlease find your login credentials below: \n\n" + "Username:  %s \n" + "Security Code: %s \n\n"
+							+ "Kindly use the above credentials to access the Sphinx application and commence your examination.\n\nShould you require any assistance, please do not hesitate to contact the administrator."
+							+ "Best regards,\nSphinx Administrator";
+ 
+			emailContext.put("contentType", "text/plain");
+			
+			
+			for (GenericValue assignedUser : assignedUsersListsWithEmails) {
+				
+				emailContext.put("sendTo", assignedUser.getString("infoString"));
+				emailContext.put("body",
+								String.format(emailBody, assignedUser.getString("userLoginId"), assignedUser.getString("passwordChangesAuto")));
+			}
+			
+			try {
+				dispatcher.runAsync("sendMail", emailContext);
+			}catch(GenericServiceException e) {
+				e.printStackTrace();
+				return ServiceUtil.returnError("Failed To Send an Exam Notification "+e.getMessage());
+			}
+			
+			return ServiceUtil.returnSuccess("Mail Notificaiton Initiated! The Users will recieve the Email shortly!");
+		}catch(Exception e) {
+			e.printStackTrace();
+			return ServiceUtil.returnError(e.getMessage());
+		}
+	}
+}
