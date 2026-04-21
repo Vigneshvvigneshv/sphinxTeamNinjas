d9b4f7199e808bd98e236eef361148e0fe84a611
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-04-21 11:31:47 +0530

    get the user rresult

---
+++ src/main/java/com/vastpro/sphinx/rest/resource/ExamResultResource.java
@@ -0,0 +1,42 @@
+package com.vastpro.sphinx.rest.resource;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.ofbiz.service.LocalDispatcher;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+
+@Path("/exam-result")
+public class ExamResultResource {
+	
+	@POST
+	@Path("/getexam-result")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getResult(@Context HttpServletRequest request,@Context HttpServletResponse response) {
+		LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+		try {
+			Map<String, Object> input = new HashMap<String, Object>();
+			input.put("partyId", request.getAttribute("partyId"));
+			input.put("examId", request.getAttribute("examId"));
+			Map<String, Object> result = dispatcher.runSync("getExamResult", input);
+			return Response.ok(result).build();
+
+		} catch (Exception e) {
+			return Response.status(500).entity(Map.of("error", e.getMessage())).build();
+		}
+	}
+	
+	
+
+}
