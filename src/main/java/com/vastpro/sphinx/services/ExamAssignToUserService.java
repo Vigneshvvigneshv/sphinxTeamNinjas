adbbee1e3c8ea1d5b4be4b393ebfdd379537a173
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-05-07 15:52:01 +0530

    add the validation before assign the user to exam(setup)

--- src/main/java/com/vastpro/sphinx/services/ExamAssignToUserService.java
+++ src/main/java/com/vastpro/sphinx/services/ExamAssignToUserService.java
@@ -40,7 +40,16 @@ public class ExamAssignToUserService {
 		LocalDispatcher dispatcher = context.getDispatcher();
 		Delegator delegator=context.getDelegator();
 		String examId = (String) input.get("examId");
-
+		
+		try {
+			Long examSetup=EntityQuery.use(delegator).from("ExamMaster").where("examId",examId).queryFirst().getLong("examSetupProper");
+			if(examSetup==0) {
+				return ServiceUtil.returnError("Please, setup exam before assign the user's");
+			}
+		} catch (GenericEntityException e) {
+			Debug.logError(e.getMessage(), ExamAssignToUserService.class.getName());
+			return ServiceUtil.returnError("Users can't assign to the exam, Please Try again later");
+		}
 		List<Map<String, Object>> assignedUser = (List<Map<String, Object>>) input.get("assignedUserList");
 		if (assignedUser != null && assignedUser.size() > 0) {
 			try {
